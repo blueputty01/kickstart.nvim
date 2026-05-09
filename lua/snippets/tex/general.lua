a4365b4ca823ef3454199be12a99029b6ac71930
Alexander Yang <65861156+blueputty01@users.noreply.github.com>
2026-05-09 13:18:21 -0400

    add ie eg

---
+++ lua/snippets/tex/general.lua
@@ -0,0 +1,4 @@
+return {
+  s({ trig = ';ie', condition = in_mathzone, snippetType = 'autosnippet' }, fmta('i.e.', {})),
+  s({ trig = ';eg', condition = in_mathzone, snippetType = 'autosnippet' }, fmta('e.g.', {})),
+}
